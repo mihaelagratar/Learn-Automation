f7d73a60c88675d4c83dde1f2009c505d49735f6
Mihaela Gratar <mihaela.gratar@3pillarglobal.com>
2020-07-08 16:53:01 +0300

    add ee.py file to repo
    :q
    :w

---
+++ aa.py
@@ -0,0 +1,24 @@
+message = "Please shut up"
+print(message)
+
+print ("division")
+print(17 / 3)
+
+print ("multiplication")
+print(2*4)
+print(9-5)
+print(87+324)
+print(5.7/3.8)
+print(7.34*4.97)
+print(23432.343-32423.3432)
+print(1234.34+3.3)
+
+a= 3.5
+b= 2.8
+print(a*b)
+
+a=7
+b=9
+print(a/b)
+
+print("alexa")
---
+++ ee.py
@@ -0,0 +1,13 @@
+#1
+#vanilla - syntax error
+print("vanilla")
+print()
+
+#3
+quote = "It is better to be hated for what you are than to be loved for what you are not."
+print (quote + "\nCredits to : Andre Gide")
+print()
+
+#2
+name = input("Mihaela Gratar")
+#pressing Enter key will finish the program
